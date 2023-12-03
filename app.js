79b8a778c8b07d6612a042bd9e459c12e2e8379d
Sohil yeole <yeolesohil44@gmail.com>
2023-12-03 15:56:57 +0530

    cors

--- app.js
+++ app.js
@@ -19,10 +19,12 @@ app.use(bodyParser.json())
 
 app.use(express.json())
 
-app.use(cors({
-    origin:[process.env.FRONTEND_URL], 
-    credentials:true
-}));
+// app.use(cors({
+//     origin:[process.env.FRONTEND_URL], 
+//     credentials:true
+// }));
+app.use(cors());
+
 
 app.use(cookieParser());
 app.use(morgan('dev'));
