0cb1dd45983e68c96b9a137813a974b0ff0a5ada
Sohil yeole <yeolesohil44@gmail.com>
2023-12-04 00:32:53 +0530

    main

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ app.use(express.json())
 //     credentials:true
 // }));
 app.use(cors({
-    origin:"http://localhost:5173", 
+    origin:"https://lms-19158912.netlify.app", 
     credentials:true
 }));
 // app.use(cors({
